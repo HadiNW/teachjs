5f6e4575d0a60fd7049a5ad01c17aeac984955e3
Hadi Nur Wahid <hadi.wahid@shipper.id>
2023-09-21 16:32:39 +0700

    add task

---
+++ conditionalTasks.js
@@ -0,0 +1,14 @@
+// Diketahui seorang user membeli timun, cabai dan tomat masing2 seharga. Rp10.000, Rp21.000, Rp15.000. 
+// dengan diskon 10% jika membeli 1 barang saja. 
+// user akan mendapatkan diskon 20% jika membeli 2 barang. 
+// user akan mendapatkan diskon 30% jika membeli 3 barang atau lebih. 
+// berapakah uang yg dikeluarkan user jika terdapat PPN 11%
+
+// jika membeli tomat lebih dari 2kg akan mendapat diskon 15%
+// jika membeli tomat lebih dari 3kg akan mendapat diskon 15%
+// jika membeli tomat lebih dari 4kg akan mendapat diskon 15%
+
+
+function shop (tomatoQty, chilliQty, tomatooQty) {
+
+}
